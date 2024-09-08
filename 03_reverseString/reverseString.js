191105ac0bcb432239d1dfbdf608e1100be3a7fb
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-07 20:27:26 -0400

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = '';
+    for (let i = 0; i < input.length; i++){
+        output += input.at(-1-i);
+    }
+    return output;
 };
 
 // Do not edit below this line
