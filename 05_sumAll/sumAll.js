05f64e639e823b25f2827655355f7cf2c467a801
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-07 21:45:02 -0400

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+    if (startInt < 0 || endInt < 0 ||
+        typeof(startInt) !== "number" || typeof(endInt) !== "number" ||
+        startInt % 1 !== 0 || endInt % 1 !== 0
+        ){
+        return "ERROR";
+    }
+    
+    let output = 0;
+    if(startInt <= endInt){
+        for(let i = startInt; i <= endInt; i++){
+        output += i;
+        }
+    } else {
+        for(let i = endInt; i <= startInt; i++){
+            output += i;
+            }
+    }
+    
+    return output;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
