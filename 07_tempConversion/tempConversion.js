a51d8ab05b7adeb19e513e963aea837a5f535e8d
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-07 22:15:46 -0400

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let output = (temperature - 32) * (5/9);
+  if(output % 1 !== 0) {
+    output = output.toFixed(1);
+  }
+  return +output;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let output = (temperature * (9/5)) +32;
+  if(output % 1 !== 0) {
+    output = output.toFixed(1);
+  }
+  return +output;
 };
 
 // Do not edit below this line
