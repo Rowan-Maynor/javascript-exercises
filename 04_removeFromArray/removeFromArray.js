a1254e0b8667b0f163eb8e6e87713c53fe507c05
Rowan Maynor <rowanmaynor@gmail.com>
2024-09-07 21:19:34 -0400

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(input, ...filterValues) {
+    const output = [];
+    
+    filter: for(let i = 0; i < input.length; i++) {
+        for (const value of filterValues){
+            if (input[i] === value){
+                continue filter;
+            }
+        }
+        output.push(input[i]);  
+    }
+    return output;
 };
 
 // Do not edit below this line
